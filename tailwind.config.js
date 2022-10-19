9fb02e513a398b55949864faac15db5fbce0c5ae
alain-kubwayo <ahkubwayo@gmail.com>
2022-10-19 15:25:04 +0200

    add get started and global scale sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#425466",
+        secondary: "#635BFF",
+        tertiary: "#0A2540",
+        auxiliary: "#ADBCCB",
+        label: "#00D4FF",
+      },
+      fontSize: {
+        "sm-md": "15px",
+        "4.5xl": "38px",
+      },
+    },
   },
   plugins: [],
 };
